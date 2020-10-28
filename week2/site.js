1d09bf0e5910984302f510a5ec200b7efbbc81be
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-10-28 16:31:19 -0400

    creating and appending_elements

---
+++ week2/site.js
@@ -0,0 +1,32 @@
+var header = document.createElement("h1");
+header.textContent = "Welcome to my JS site";
+document.body.append(header);
+
+var par = document.createElement("p");
+par.textContent = "All of this was created with JavaScript";
+document.body.append(par);
+
+
+
+var listItem = document.createElement("ol");
+listItem.textContent = "Learn About Javascritp";
+
+
+var firstList = document.createElement("li");
+firstList.textContent = "Learn";
+listItem.append(firstList);
+
+
+var secondList = document.createElement("li");
+secondList.textContent = "About";
+listItem.append(secondList);
+
+var thirdList = document.createElement("li");
+thirdList.textContent = "Javascript";
+listItem.append(thirdList);
+
+
+
+
+document.body.append(listItem);
+
