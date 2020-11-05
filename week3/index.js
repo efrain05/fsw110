1a4337ddfc388cf140d4797979c922b465c5080a
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-11-05 14:55:33 -0500

    creating_a_lis

---
+++ week3/index.js
@@ -0,0 +1,30 @@
+for (var i = 0; i < 10; i++) {
+    var head = document.createElement("h1");
+    head.innerHTML = "Hello World";
+
+    document.body.appendChild(head);
+    
+}
+
+var style = document.createElement("div");
+style.innerHTML = "<br><style>h1 { color: red; font-size:20px }</style>";
+document.body.appendChild(style);
+
+const names = [
+    "steve",
+    "larry",
+    "joe",
+    "shirley",
+    "steph",
+    "nate",
+    "rick",
+    "emily",
+
+]
+
+
+var list;
+for (list of names) {
+    document.write(list + "<br >");
+  }
+
