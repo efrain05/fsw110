45c58b624e16042a9b0d90f9c8e62a563e1e99ce
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-11-06 22:29:30 -0500

    changing_styles_wJS

--- week3/style.js
+++ week3/style.js
@@ -2,16 +2,29 @@ for (var i = 0; i < 5; i++) {
     var head = document.createElement("h2");
     head.innerHTML = "Hello World";
     document.body.appendChild(head);
-    
+   
 }
+
+
 
 var style = document.createElement("div");
 style.innerHTML = "<br><style>h2 {font-size:20px;font-weight: lighter; font-family: sans-serif; color: cornflowerblue;}</style>";
+
 document.body.appendChild(style);
 
 
-var css = document.querySelector("h2") 
-css.classList.add("border")
+
+
+
+var css = document.querySelectorAll("h2");
+for (var i = 0; i < 5; i++){
+css[i].classList.add("border");
+
+}
+
+
+
+
 
 
 
