e745b39c4529c2042682476cedca442af1ad4eee
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-11-18 00:10:10 -0500

    submit_events

---
+++ week5/mod.js
@@ -0,0 +1,12 @@
+
+  function fillThisOut() {
+
+    var fullName= document.getElementById("first").value;
+    var age = document.getElementById("second").value;
+    var email = document.getElementById("third").value;
+    return alert("Full Name:" + fullName + " " + "Age is:" + age + " " + "Email is:" + email)
+    document.getElementById("new").reset();
+   
+  } 
+ 
+
