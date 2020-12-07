bdfce6a2ed778721cc814fdaf36a515c2f068c79
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-12-06 22:47:55 -0500

    capstone_project

--- week7/pro.js
+++ week7/pro.js
@@ -1,36 +1,19 @@
-
-let form = document.getElementById("todo");
 let list = document.getElementById("myList");
 let input = document.getElementById("add1");
-let input2 = document.getElementById("add2")
+let input2 = document.getElementById("add2");
 let button = document.getElementById("button");
 let id = 1;
 
-button.addEventListener("click", addToDo)
-list.addEventListener("click", removeEvent)
 
-
-function addToDo (e) {
+function addToDo() {
     let text = input.value;
     let textAdd = input2.value;
     let item = `<li class="del">
-    ${text} ============= ${textAdd} <button class="del">Delete</button>`
-    list.insertAdjacentHTML("beforeend",item);
-    id++; 
+    ${text} ============= ${textAdd} <button class="del">Delete</button>`;
+
+    document.querySelector("#todo").insertAdjacentHTML("beforeend", item);
     document.getElementById("add1").value = "";
     document.getElementById("add2").value = "";
-    
 
-}
-
-
-function removeEvent(e) {
-    if(e.target.classList.contains("del")) {
-        list.removeChild(e.target.parentElement);
-        list.removeChild(list);
-        
-    }
   }
-
-
 
