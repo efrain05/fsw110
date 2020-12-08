dd80dafbde6dba4cf1cd651d373e752b0b62be80
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-12-07 23:32:56 -0500

    capstone_project

--- week7/pro.js
+++ week7/pro.js
@@ -4,16 +4,24 @@ let input2 = document.getElementById("add2");
 let button = document.getElementById("button");
 let id = 1;
 
+list.addEventListener("click", removeEvent)
 
 function addToDo() {
     let text = input.value;
     let textAdd = input2.value;
     let item = `<li class="del">
     ${text} ============= ${textAdd} <button class="del">Delete</button>`;
-
-    document.querySelector("#todo").insertAdjacentHTML("beforeend", item);
+    list.insertAdjacentHTML("beforeend", item);
     document.getElementById("add1").value = "";
     document.getElementById("add2").value = "";
 
   }
 
+
+function removeEvent(e) {
+    if(e.target.classList.contains("del")) {
+        list.removeChild(e.target.parentElement);
+        list.removeChild(list);
+        
+    }
+  }
